1e6f4ff4a3952d3e4a8807706e98602e96393844
mihail <tarakanov.m.a@mail.ru>
2016-06-05 22:15:40 +0300

    creat class HugeIntegers

---
+++ 9/HugeInteger/Huge.h
@@ -0,0 +1,18 @@
+#ifndef HUGE_H
+#define HUGE_H
+
+class HugeInteger {
+public:
+	HugeInteger();
+	void setHG();
+	void getHG();
+	void input();
+	void output();
+	void add();
+	void substract();
+private:
+	int hg[ 40 ];
+};
+
+
+#endif
